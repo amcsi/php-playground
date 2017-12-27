bb249bc5fd6bb10f61d967a281475d688533476a
Attila Szeremi <attila@szeremi.org>
2017-12-27 18:52:08 +0100

    Add Class1...9Test.php

---
+++ test/Class2Test.php
@@ -0,0 +1,56 @@
+<?php
+declare(strict_types=1);
+
+namespace amcsi\playground\test;
+
+use PHPUnit\Framework\TestCase;
+
+require_once __DIR__ . '/../src/Class2.php';
+
+final class Class2Test extends TestCase
+{
+    /**
+     * @dataProvider provideTest1
+     */
+    public function test1(): void
+    {
+
+    }
+
+    public function provideTest1()
+    {
+        return [
+            [],
+        ];
+    }
+
+    /**
+     * @dataProvider provideTest2
+     */
+    public function test2(): void
+    {
+
+    }
+
+    public function provideTest2()
+    {
+        return [
+            [],
+        ];
+    }
+
+    /**
+     * @dataProvider provideTest3
+     */
+    public function test3(): void
+    {
+
+    }
+
+    public function provideTest3()
+    {
+        return [
+            [],
+        ];
+    }
+}
